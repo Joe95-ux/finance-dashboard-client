8fb28eb5da8262e831fb3a4b08e719003212d8e4
joe95-ux <ogorktabi@gmail.com>
2023-11-24 22:44:11 +0100

    add dashboard ui styles

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import { useTheme } from '@emotion/react'
+import { Box } from '@mui/material'
 
 type Props = {}
+const gridTemplateLargeScreens = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+`;
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
 
 const Dashboard = (props: Props) => {
+  const {palette} = useTheme(); 
   return (
-    <div> Dashboard</div>
+    <Box width="100%" height="100%" display="grid" gap="1.5rem"
+     sx={{
+        gridTemplateAreas: gridTemplate,
+     }}>
+
+    </Box>
   )
 }
 
