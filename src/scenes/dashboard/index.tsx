34aa3e61a313f6c528b0f40bbbcb6fa18cbe829d
joe95-ux <ogorktabi@gmail.com>
2023-11-25 22:45:51 +0100

    implement dashboard styles for different sections of dasboard

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,5 +1,8 @@
-import { useTheme } from '@emotion/react'
-import { Box } from '@mui/material'
+import { Box, useMediaQuery } from '@mui/material'
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
+
 
 type Props = {}
 const gridTemplateLargeScreens = `
@@ -47,16 +50,34 @@ const gridTemplateSmallScreens = `
   "j"
 `;
 
-const Dashboard = (props: Props) => {
-  const {palette} = useTheme(); 
-  return (
-    <Box width="100%" height="100%" display="grid" gap="1.5rem"
-     sx={{
-        gridTemplateAreas: gridTemplate,
-     }}>
-
-    </Box>
-  )
-}
+const Dashboard = () => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+    return (
+      <Box
+        width="100%"
+        height="100%"
+        display="grid"
+        gap="1.5rem"
+        sx={
+          isAboveMediumScreens
+            ? {
+                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+                gridTemplateAreas: gridTemplateLargeScreens,
+              }
+            : {
+                gridAutoColumns: "1fr",
+                gridAutoRows: "80px",
+                gridTemplateAreas: gridTemplateSmallScreens,
+              }
+        }
+      >
+        <Row1 />
+        <Row2 />
+        <Row3 />
+       
+      </Box>
+    );
+  };
 
 export default Dashboard
