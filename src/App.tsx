11325f716d1ba0c4099308b675f22af09aa8a08c
joe95-ux <ogorktabi@gmail.com>
2023-12-13 21:49:47 +0200

    add row 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import {Box, CssBaseline, ThemeProvider} from "@mui/material";
-import {createTheme} from "@mui/material/styles";
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
-import {themeSettings} from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
 import Dashboard from "@/scenes/dashboard";
+// import Predictions from "@/scenes/predictions";
 
 function App() {
-  const theme = useMemo(()=>createTheme(themeSettings), []);
-  
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <CssBaseline/>
+          <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            <Navbar/>
+            <Navbar />
             <Routes>
-              <Route path="/" element= {<Dashboard/>}/>
-              <Route path="/predictions" element= {<div>predictions page</div>}/>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/predictions" element={<Predictions />} /> */}
             </Routes>
-             
           </Box>
-
         </ThemeProvider>
-
       </BrowserRouter>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
